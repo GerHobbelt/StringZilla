b69e26cfc99986cb3a46abde34f1d927eabb32a9
Ger Hobbelt <ger@hobbelt.com>
2025-08-16 12:41:48 +0200

    MSVC: fix compiler error about missing std::alocator definition: include the proper header file.

--- include/stringzilla/types.hpp
+++ include/stringzilla/types.hpp
@@ -94,6 +94,7 @@
 #if !SZ_AVOID_STL
 #include <initializer_list> // `std::initializer_list` is only ~100 LOC
 #include <iterator>         // `std::random_access_iterator_tag` pulls 20K LOC
+#include <memory>           // `std::allocator_traits`
 #endif
 
 namespace ashvardanian {
